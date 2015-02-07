c1e524aabd912b1e1f7aa94336e78180d00e4df2
Misumi Rize <r@ayase-e.li>
2015-02-07 11:50:50 +0900

    Use full path of brew-cask

--- lib/itamae/plugin/resource/cask.rb
+++ lib/itamae/plugin/resource/cask.rb
@@ -12,24 +12,24 @@ module Itamae
           super
           ensure_brew_cask_availability
 
-          result = run_command("brew cask list | grep '#{attributes.target}$'", error: false)
+          result = run_command("/usr/local/bin/brew-cask list | grep '#{attributes.target}$'", error: false)
           current.exist = result.exit_status == 0
         end
 
         def action_install(options)
           unless current.exist
-            run_command(["brew", "cask", "install", attributes.target])
+            run_command(["/usr/local/bin/brew-cask", "install", attributes.target])
           end
         end
 
         def action_alfred(options)
-          run_command(["brew", "cask", "alfred", attributes.target])
+          run_command(["/usr/local/bin/brew-cask", "alfred", attributes.target])
         end
 
         private
 
         def ensure_brew_cask_availability
-          if run_command("brew cask > /dev/null", error: false).exit_status != 0
+          if run_command("/usr/local/bin/brew-cask > /dev/null", error: false).exit_status != 0
             raise "`brew cask` command is not available. Please install brew cask."
           end
         end
